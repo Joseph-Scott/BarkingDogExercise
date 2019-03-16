f8b09748cf54638404a8a345f219ba5a7a2a729a
Joseph Scott <jbs198585@gmail.com>
2019-03-16 16:02:31 -0400

    test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean hours = BarkingDog.shouldWakeUp(true, 45);
+        System.out.println("hours = " + hours);
+    }
+}
